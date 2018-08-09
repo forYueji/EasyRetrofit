2960867c8b4ae8f689fa1d9922e3f5772612be37
黄亚平 <>
2018-08-09 17:28:20 +0800

    init easyRetrofit

---
+++ net/src/main/java/com/yhfund/net/api/API.java
@@ -0,0 +1,50 @@
+package com.yhfund.net.api;
+
+
+import java.util.HashMap;
+import java.util.List;
+
+import io.reactivex.Observable;
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Url;
+
+/**
+ * API Definition
+ */
+public interface API {
+
+    /**
+     * POST Request FormUrlEncoded
+     * @param url
+     * @param headerMap
+     * @param params
+     * @return
+     */
+    @FormUrlEncoded
+    @POST
+    Observable<String> post(@Url String url, @HeaderMap HashMap<String, String> headerMap,
+                            @FieldMap HashMap<String, String> params);
+
+    @Multipart
+    @POST
+    Observable<String> upload(@Url String url, @HeaderMap HashMap<String, String> headerMap,
+                              @Part List<MultipartBody.Part> files);
+
+    @POST
+    Observable<ResponseBody> downloadPicFromNetFile(@Url String url, @HeaderMap HashMap<String, String> headerMap);
+
+    @FormUrlEncoded
+    @POST
+    Observable<ResponseBody> downloadPicFromNetBM(@Url String url, @HeaderMap HashMap<String, String> headerMap,
+                                                  @FieldMap HashMap<String, String> params);
+
+    @POST
+    Observable<ResponseBody> downloadPicFromNetBM(@Url String url, @HeaderMap HashMap<String, String> headerMap);
+}
---
+++ net/src/main/java/com/yhfund/net/util/TransformerUtil.java
@@ -0,0 +1,23 @@
+package com.yhfund.net.util;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.schedulers.Schedulers;
+
+public class TransformerUtil {
+
+    public static <T> ObservableTransformer<T, T> subSchedulers() {
+        return new ObservableTransformer<T, T>() {
+            @Override
+            public ObservableSource<T> apply(@NonNull Observable<T> upstream) {
+                return upstream.subscribeOn(Schedulers.io())
+                        .unsubscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+
+}
